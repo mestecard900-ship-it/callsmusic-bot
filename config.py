6bac4927dc0cc6b3452034ba83d87e11479c44b5
mestecard900-ship-it <mestecard900@gmail.com>
2025-11-24 01:54:35 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+# ----------------------------
+# هنا بياناتك (اكتب إعدادات البوت)
+# ----------------------------
+
+SESSION_NAME = "session"
+
+# البوت توكن
+BOT_TOKEN = "7614551327:AAGHtL9QYpP4rbUdK9v_nlc1Pt5lLW7Bbuc"
+
+# api id (رقم)
+API_ID = 734531
+
+# api hash (نص)
+API_HASH = "4ac3f42230e40a27cdcbaba52d754cb2"
+
+# بالدقائق مدة الفيديو
+DURATION_LIMIT = 7
+
+# البريفكس
+COMMAND_PREFIXES = ["/", "!"]
+
+# الآي دي الخاص بك (الأدمن)
+SUDO_USERS = [6895268368]
+
+# قناة الاشتراك الإجباري بدون @
+FORCE_CHANNEL = "xx_aa_i"
+
+# جروب السجل (اللوقات)
+LOG_GROUP_ID = -1001840357767

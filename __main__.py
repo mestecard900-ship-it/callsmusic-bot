7a309e6066d942d322d65d4c312bfe58024636fa
mestecard900-ship-it <mestecard900@gmail.com>
2025-11-24 03:06:47 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,16 +1,16 @@
 from pyrogram import Client as Bot
 
-from .callsmusic import run
-from .config import API_HASH
-from .config import API_ID
-from .config import BOT_TOKEN
+from callsmusic import run
+from config import API_HASH, API_ID, BOT_TOKEN
 
 
-Bot(
-    ':memory:',
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={'root': 'callsmusic.handlers'},
-).start()
-run()
+if __name__ == "__main__":
+    Bot(
+        ":memory:",
+        API_ID,
+        API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins={"root": "callsmusic.handlers"},
+    ).start()
+
+    run()
